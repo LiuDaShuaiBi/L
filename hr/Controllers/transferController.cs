0049bb73d531e03edc13f70a57c6f4cc7a1e5eb6
LiuDaShuaiBi <863435381@qq.com>
2018-09-11 19:17:57 +0800

    1

--- hr/Controllers/transferController.cs
+++ hr/Controllers/transferController.cs
@@ -16,6 +16,7 @@ namespace hr.Controllers
         // GET: transfer
         public ActionResult check()
         {
+            string lksb = "";
             return View();
         }
         public ActionResult check_list()
